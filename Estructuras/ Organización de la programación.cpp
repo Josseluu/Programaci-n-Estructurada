9f8ba71bf58f33dc66e77d65a578816700b2fc73
Josseluu <jslslpz@myuax.com>
2023-12-12 18:01:50 +0100

    Initial commit

--- Estructuras/ Organización de la programación.cpp
+++ Estructuras/ Organización de la programación.cpp
@@ -1,24 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 struct Estudiante {
-    char nombre[50];
+    std::string nombre;
     int edad;
-    float promedio;
+
+    Estudiante(const std::string& nombre, int edad) : nombre(nombre), edad(edad) {}
 };
 
+void imprimirEstudiantes(const std::vector<Estudiante>& estudiantes) {
+    std::cout << "Lista de Estudiantes:\n";
+    for (const auto& estudiante : estudiantes) {
+        std::cout << "Nombre: " << estudiante.nombre << ", Edad: " << estudiante.edad << "\n";
+    }
+    std::cout << "\n";
+}
 
-struct Nodo {
-    struct Estudiante estudiante;
-    struct Nodo *siguiente;
-};
+void eliminarEstudiantePorNombre(std::vector<Estudiante>& estudiantes, const std::string& nombre) {
+    auto it = std::remove_if(estudiantes.begin(), estudiantes.end(),
+                             [nombre](const Estudiante& estudiante) {
+                                 return estudiante.nombre == nombre;
+                             });
+    estudiantes.erase(it, estudiantes.end());
+}
 
+int main() {
+    std::vector<Estudiante> estudiantes;
 
-void agregarEstudiante(struct Nodo **lista, struct Estudiante estudiante) {
+    estudiantes.push_back(Estudiante("Juan", 20));
+    estudiantes.push_back(Estudiante("Maria", 22));
+    estudiantes.push_back(Estudiante("Carlos", 21));
 
-    struct Nodo *nuevoNodo = (struct Nodo *)malloc(sizeof(struct Nodo));
-    if (nuevoNodo == NULL) {
-        printf("Error: No se pudo asignar memoria\n");
-        return;
+    imprimirEstudiantes(estudiantes);
+
+    std::string nombreEliminar = "Maria";
+    eliminarEstudiantePorNombre(estudiantes, nombreEliminar);
+
+    imprimirEstudiantes(estudiantes);
+
+    return 0;
+}
