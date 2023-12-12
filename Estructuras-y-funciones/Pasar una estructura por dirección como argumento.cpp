a33bbbf761a6b9c1ab2fc8c2067e3e5618c41a0d
Josseluu <jslslpz@myuax.com>
2023-12-12 18:40:03 +0100

    Initial commit

--- Estructuras-y-funciones/Pasar una estructura por dirección como argumento.cpp
+++ Estructuras-y-funciones/Pasar una estructura por dirección como argumento.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 #include <string>
 
-// Definición de la estructura Estudiante
+
 struct Estudiante {
     std::string nombre;
     int edad;
     float promedio;
 };
 
-// Función que imprime los miembros de la estructura Estudiante a través de un puntero
+
 void imprimirEstudiante(const Estudiante* estudiante) {
     if (estudiante == nullptr) {
         std::cout << "Error: El puntero es nulo." << std::endl;
@@ -21,13 +21,12 @@ void imprimirEstudiante(const Estudiante* estudiante) {
 }
 
 int main() {
-    // Crear una instancia de la estructura Estudiante
+
     Estudiante estudiante1 = {"Juan", 20, 8.5};
 
-    // Obtener la dirección de la estructura Estudiante
+
     Estudiante* punteroEstudiante = &estudiante1;
 
-    // Llamar a la función para imprimir los miembros del estudiante mediante un puntero
     imprimirEstudiante(punteroEstudiante);
 
     return 0;
