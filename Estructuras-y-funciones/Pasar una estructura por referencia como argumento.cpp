44216d246e2c54cabe372d3a7d4af27f11fa3ab4
Josseluu <jslslpz@myuax.com>
2023-12-12 18:29:56 +0100

    Initial commit

---
+++ Estructuras-y-funciones/Pasar una estructura por referencia como argumento.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+
+struct Estudiante {
+    std::string nombre;
+    int edad;
+    float promedio;
+};
+
+
+void imprimirEstudiante(const Estudiante& estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+}
+
+int main() {
+
+    Estudiante estudiante1 = {"Juan", 20, 8.5};
+
+
+    imprimirEstudiante(estudiante1);
+
+    return 0;
+}
