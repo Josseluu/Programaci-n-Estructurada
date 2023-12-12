08c61e351a3beb751ccbd85ce444fce5a6bf1ff4
Josseluu <jslslpz@myuax.com>
2023-12-12 11:51:20 +0100

    Initial commit

--- Estructuras/Instanciación de estructuras.cpp
+++ Estructuras/Instanciación de estructuras.cpp
@@ -1,1 +1,25 @@
 #include <iostream>
+#include <string>
+
+struct Estudiante {
+    std::string nombre;
+    int edad;
+    float promedio;
+};
+
+int main() {
+
+    Estudiante estudiante1;
+
+
+    estudiante1.nombre = "Ana García";
+    estudiante1.edad = 22;
+    estudiante1.promedio = 9.2;
+
+
+    std::cout << "Nombre: " << estudiante1.nombre << std::endl;
+    std::cout << "Edad: " << estudiante1.edad << std::endl;
+    std::cout << "Promedio: " << estudiante1.promedio << std::endl;
+
+    return 0;
+}
